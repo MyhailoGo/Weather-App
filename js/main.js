64eae35c13482a2669f5754bb4b338e7e8238cc4
MyhailoGo <myhailoustyak@ukr.net>
2021-03-05 15:23:20 +0200

    new chandges

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const options = {
 };
 
 
-addWeatherItem();
+addWeatherItem(); // створюємо другорядні елементи для показу погоди на наступні години
 selectCity("lutsk");
 
 
@@ -17,16 +17,16 @@ buttonsCities.forEach((cityButton) => {
 
 function selectCity(cityName) {
   
-  getTimeOfDay(new Date().getHours());
+ 
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=111d8d44f58751fb5cc404f71f53e329`
   )
     .then((res) => res.json())
     .then((res) => {
      
-      getDate((res.timezone-7200)/3600);
-      let weatherDescrip = weatherCondition(res.weather[0].description);
-      addImgIcons(options.timeOfTheDayNow,weatherDescrip);
+      getDate(res.timezone-7200); // віднімаємо різницю в часовому поясі, аби передати 0-ий часовий пояс
+      let weatherDescrip = getWeatherCondition(res.weather[0].description); // отримуємо описання погоди(ясно, дощ і т.д.)
+      addImgIcons(options.timeOfTheDayNow, weatherDescrip); // додаємо картинку погоди в карточку.
       
       document.querySelector(".temp").innerText =
         Math.round(res.main.temp - 273) + " °C";
@@ -40,7 +40,7 @@ function selectCity(cityName) {
     .catch((err)=>alert("Не вдалося отримати дані з сервера"));
 
     fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=111d8d44f58751fb5cc404f71f53e329&lang=uk`
+      `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=111d8d44f58751fb5cc404f71f53e329`
     )
       .then((res) => res.json())
       .then((res) => {
@@ -53,25 +53,27 @@ function selectCity(cityName) {
 
 
 function getDate(timeZone) {
-    const d = new Date(+new Date() + +timeZone*3600000);
+    const d = new Date(+new Date() + +timeZone*1000); // отримуємо дату з урахуванням часового поясу
 
+    options.timeOfTheDayNow = getTimeOfDay(d.getHours());  // отримуєто пору дня(день чи ніч)
+    
     const day = ["Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"];
     const month = ["Січня","Лютого","Березня","Квітня","Травня","Червня","Липня","Серпня","Вересня","Жовтня","Листопада","Грудня"]
     const hours = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours(); 
     const minutes = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
     
     document.querySelector(".date").innerText = 
-    `${d.getDate()} ${month[d.getMonth()]}, ${day[d.getDay()]}, ${hours}:${minutes}`;
+    `${d.getDate()} ${month[d.getMonth()]}, ${day[d.getDay()]}, ${hours}:${minutes}`; // добавляємо дату в карту погоди
 
   
 };
 
 
 function getTimeOfDay(timeNow){
-    timeNow>4 && timeNow<19 ? options.timeOfTheDayNow ="day" : options.timeOfTheDayNow ="night";
+  return timeNow>4 && timeNow<19 ? "day" : "night";
 };
 
-function weatherCondition(weather){
+function getWeatherCondition(weather){
     let weatherNow = "";
     switch(weather){
       case "clear sky": weatherNow = "clear";
@@ -117,18 +119,21 @@ function addImgIcons(timeOfTheDay,weatherNow){
 
 function addWeatherItem(){
   fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?q=lutsk&appid=111d8d44f58751fb5cc404f71f53e329&lang=uk`
+    `http://api.openweathermap.org/data/2.5/forecast?q=lutsk&appid=111d8d44f58751fb5cc404f71f53e329`
   )
     .then((res) => res.json())
     .then((res) => {
-      for(let i = 0; i<9; i++){
+      for(let i = 0; i<12; i++){
         const div = document.createElement("div");
         div.classList.add("weather-item");
 
         div.innerHTML =  `
         <div class="time-item">
-            ${res.list[i].dt_txt.split(" ")[0]}	
-            ${res.list[i].dt_txt.split(" ")[1].slice(0,-3)}		
+            <span> ${res.list[i].dt_txt.split(" ")[0]}	</span>
+            <span class="hours-item"> ${res.list[i].dt_txt.split(" ")[1].slice(0,-3)} </span>		
+        </div>
+        <div class="img-item-block">
+            <img class="img-item"></img>
         </div>
         <div class="temp-item">
             ${Math.round(res.list[i].main.temp - 273) +" °C"}
@@ -140,10 +145,15 @@ function addWeatherItem(){
 }
 
 function changeWeatherItem(res){
-  for(let i = 0; i<9; i++){
+  for(let i = 0; i<12; i++){
+    const timeOfTheDay = getTimeOfDay(new Date(res.list[i].dt_txt).getHours());
+    const weatherNow = getWeatherCondition(res.list[i].weather[0].description);
+
     document.querySelectorAll(".time-item")[i].innerHTML =  `
-      ${res.list[i].dt_txt.split(" ")[0]}	
-      ${res.list[i].dt_txt.split(" ")[1].slice(0,-3)}`;
+    <span> ${res.list[i].dt_txt.split(" ")[0]}	</span>
+    <span class="hours-item"> ${res.list[i].dt_txt.split(" ")[1].slice(0,-3)} </span> `;
+
+      document.querySelectorAll(".img-item")[i].src = `img/${timeOfTheDay}/icons-${weatherNow}.png`;
     
       document.querySelectorAll(".temp-item")[i].innerHTML = 
         Math.round(res.list[i].main.temp - 273) +" °C";
